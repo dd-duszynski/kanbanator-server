577d074bedca88bff0f5c368ff7f61c86c518d3e
Damian Duszyński <dd.duszynski@gmail.com>
2021-01-11 21:29:55 +0100

    new Dump added

--- Dump/kanbanator_boards_lists.sql
+++ Dump/kanbanator_boards_lists.sql
@@ -23,11 +23,12 @@ DROP TABLE IF EXISTS `boards_lists`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `boards_lists` (
-  `list_id` int NOT NULL DEFAULT '0',
+  `list_id` int NOT NULL AUTO_INCREMENT,
   `list_title` varchar(30) NOT NULL,
   `list_privacy` varchar(30) NOT NULL,
-  `list_related_board` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `list_related_board` int NOT NULL,
+  PRIMARY KEY (`list_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,7 +37,7 @@ CREATE TABLE `boards_lists` (
 
 LOCK TABLES `boards_lists` WRITE;
 /*!40000 ALTER TABLE `boards_lists` DISABLE KEYS */;
-INSERT INTO `boards_lists` VALUES (70,'Backlog','private',1),(71,'Preparing Content','private',1),(72,'Awaiting Review','private',1),(75,'Planned','private',4),(76,'To Do','private',4),(77,'Doing','private',4),(80,'USEFUL STUFF','private',6),(81,'Draft','private',6),(82,'PENDING EDITING','private',6),(84,'TO DO','private',7),(85,'BLOCKING','private',7),(86,'IN PROGRESS','private',7);
+INSERT INTO `boards_lists` VALUES (70,'Backlog','private',1),(71,'Preparing Content','private',1),(72,'Awaiting Review','private',1),(75,'Planned','private',4),(76,'To Do','private',4),(77,'Doing','private',4),(80,'USEFUL STUFF','private',6),(81,'Draft','private',6),(82,'PENDING EDITING','private',6),(84,'TO DO','private',7),(85,'BLOCKING','private',7),(86,'IN PROGRESS','private',7),(87,'Done!','private',1),(88,'card x4','private',7),(89,'Test','private',7),(90,'Test2','private',7),(91,'Test1','private',4),(92,'Test2','private',4),(93,'Ok','private',4),(94,'Test dla Oli','private',4),(95,'Test','private',4),(96,'nowa lista','private',1),(97,'Lista 2','private',20),(98,'Lista 3','private',20),(99,'Lista1','private',20),(100,'Kolejna','private',20),(101,'Lista 3','private',18),(102,'Test','private',18),(103,'Oki','private',19),(104,'Oki2','private',19),(105,'Oki','private',26),(106,'Wskoczy na miejsce defaulta','private',25),(107,'Jeszcze raz','private',24),(108,'ok','private',22),(109,'ok2','private',22),(110,'a','private',32),(111,'b','private',32),(112,'Lista testowa','private',123123),(113,'Lista testowa2','private',123123),(115,'Default Title','private',141516);
 /*!40000 ALTER TABLE `boards_lists` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-07 19:17:00
+-- Dump completed on 2021-01-11 21:29:13
